1c637c3413ce2f6025e712fcf8187bfae57c531d
mohammadzaidibrahim-create <mohammadzaidibrahim@gmail.com>
2026-04-24 21:51:05 +0530

    Test 10 Registry Add

--- data_registry.js
+++ data_registry.js
@@ -132,5 +132,18 @@ const ALL_TESTS = [
     difficulty: "Medium",
     live: true, 
     data: TEST_JAN25_09_S2_P1  
-  } 
+  }, 
+  {
+    id: "jan25-9-s1-p1",
+    category: "p1",
+    name: "UGC NET Paper 1 (9 January 2025 - Morning)",
+    label: "PYQ · 9 Jan '25 (Morning)",
+    subject: "General Paper 1",
+    totalQs: 50,
+    marks: 100,
+    minutes: 60,
+    difficulty: "Medium",
+    live: true, 
+    data: TEST_JAN25_09_S1_P1  
+  }, 
 ];
